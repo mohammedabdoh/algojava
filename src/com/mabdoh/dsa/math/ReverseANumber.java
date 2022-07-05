142c443760acc0fa4ada114b42be1f841006d351
Mohammed Abdoh <36000898+mohammedabdoh@users.noreply.github.com>
2022-07-05 19:37:06 +0000

    Math set 1

---
+++ src/com/mabdoh/dsa/math/ReverseANumber.java
@@ -0,0 +1,14 @@
+package com.mabdoh.dsa.math;
+
+public class ReverseANumber {
+    public static int solution(int num) {
+        int rev = 0;
+
+        while (num > 0) {
+            rev = (rev * 10) + (num % 10);
+            num /= 10;
+        }
+        
+        return rev;
+    }
+}
